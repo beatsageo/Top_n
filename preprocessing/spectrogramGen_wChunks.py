d0816b707d553764478fa2562f485199332a6131
BrandonNguyenOSU <135754216+BrandonNguyenOSU@users.noreply.github.com>
2025-02-02 17:59:11 -0800

    Updated to handle crashes

--- preprocessing/spectrogramGen_wChunks.py
+++ preprocessing/spectrogramGen_wChunks.py
@@ -1,5 +1,6 @@
 import librosa
 import librosa.display
+import soundfile as sf
 import numpy as np
 import os
 import matplotlib.pyplot as plt
@@ -8,10 +9,10 @@ print(os.getcwd())
 
 # Get audio file directory
 try:
-    from_dir = input("\nPlease input the name of the folder with your audio files.\nInsert '~' to automatically append the working directory.\n>")
+    from_dir = input("\nPlease input the name of the folder with your audio files.\nInsert '.' to automatially append the working directory.\n>")
     if from_dir == "":
-        from_dir = "~/audio-files"
-    if from_dir[0] == "~":
+        from_dir = "./audio-files"
+    if from_dir[0] == ".":
         from_dir = os.getcwd() + from_dir[1:]
     songs = os.listdir(from_dir)
     print(f"{from_dir} Directory Found")
@@ -21,10 +22,10 @@ except FileNotFoundError:
 
 # Get spectrogram file directory
 try: 
-    to_dir = input("\nInput a name for your spectrograms' destination directory.\nInsert '~' to automatically append the working directory.\n>")
+    to_dir = input("\nInput a name for your spectrograms' destination directory.\nInsert '.' to automatically append the working directory.\n>")
     if to_dir == "":
         to_dir = "spec-files"
-    if to_dir[0] == "~":
+    if to_dir[0] == ".":
         to_dir = os.getcwd() + to_dir[1:]
     os.mkdir(to_dir)
     print(f"Made {to_dir} Directory")
@@ -32,9 +33,10 @@ except FileExistsError:
     print(f"{to_dir} Directory Found")
 
 # Create and save spectrogram images
-songs = list(song for song in songs if song.endswith((".au",".mp3",".m4a",".mp4",".webm")))
+songs = list(song for song in songs if song.endswith((".au",".mp3",".mp4","m4a",".webm")))
 numSongs = len(songs)
 resCount = 0
+excList = []
 print(f"Processing {numSongs} audio files...")
 
 chunk_duration = 30
@@ -46,23 +48,29 @@ for i in range(0,numSongs):
     bar = "█" * filled + '-' * (25 - filled)
     print(f'\r[{bar}]  {percent}%  {i+1}/{numSongs}', end = '\r')
 
-    # Seperate into chunks
+    # Check for error
     songPath = os.path.join(from_dir,songs[i])
-    songLen = librosa.get_duration(path=songPath)
-    numChunks = int(np.ceil(songLen / chunk_duration))
-    offset = 0
+    try:
+        # Seperate into chunks
+        songLen = librosa.get_duration(path=songPath)
+        numChunks = int(np.ceil(songLen / chunk_duration))
+        offset = 0
 
-    # Spectrogram Generation
-    for j in range(1, numChunks):
-        y, sr = librosa.load(songPath, offset=offset, duration=chunk_duration)
-        S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
-        fig, ax = plt.subplots()
-        S_dB = librosa.power_to_db(S, ref=np.max)
-        img = librosa.display.specshow(S_dB, x_axis='time', y_axis='mel', sr=sr, fmax=8000, ax=ax)
-        fig.colorbar(img, ax=ax, format='%+2.0f dB')
-        plt.savefig(f"{os.path.join(to_dir,songs[i])}_{j}.jpg")
-        plt.close()
-        offset += chunk_duration
-        resCount += 1
+        # Spectrogram Generation
+        for j in range(1, numChunks):
+            y, sr = librosa.load(songPath, offset=offset, duration=chunk_duration)
+            S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
+            fig, ax = plt.subplots()
+            S_dB = librosa.power_to_db(S, ref=np.max)
+            img = librosa.display.specshow(S_dB, x_axis='time', y_axis='mel', sr=sr, fmax=8000, ax=ax)
+            fig.colorbar(img, ax=ax, format='%+2.0f dB')
+            plt.savefig(f"{os.path.join(to_dir,songs[i])}_{j}.jpg")
+            plt.close()
+            offset += chunk_duration
+            resCount += 1
+    except Exception as e:        
+        excList.append(os.path.basename(songPath))
+        pass
 
 print(f"\n{resCount} audio files were produced...")
+print(f"These audio files failed to be processed: {excList}")
