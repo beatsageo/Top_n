92635a81777f67d5e7580cfd61846406a96ba0f4
BrandonNguyenOSU <135754216+BrandonNguyenOSU@users.noreply.github.com>
2025-02-08 17:13:42 -0800

    Added error handling and logging

--- preprocessing/export_mfccs.py
+++ preprocessing/export_mfccs.py
@@ -3,14 +3,15 @@ import os
 import math
 import librosa
 
-DATASET_PATH = "preprocessing\MiniDataset"
-JSON_PATH = "preprocessing\data.json"
+DATASET_PATH = "MiniDataset"
+JSON_PATH = "data.json"
+LOG_PATH = "log.json"
 SAMPLE_RATE = 22050
 TRACK_DURATION = 30  # measured in seconds
 SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
 
 
-def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, num_segments=5):
+def save_mfcc(dataset_path, json_path, log_path, num_mfcc=13, n_fft=2048, hop_length=512, num_segments=5):
     """Extracts MFCCs from music dataset and saves them into a json file along with genre labels.
 
         :param dataset_path (str): Path to dataset
@@ -27,6 +28,10 @@ def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, 
         "mapping": [],
         "labels": [],
         "mfcc": []
+    }
+    log = {
+        "failCount": 0,
+        "failed": []
     }
 
     samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
@@ -48,29 +53,36 @@ def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, 
 
                 # load audio file
                 file_path = os.path.join(dirpath, f)
-                signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE)
+                try:
+                    signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE)
 
-                # process all segments of audio file
-                for d in range(num_segments):
+                    # process all segments of audio file
+                    for d in range(num_segments):
 
-                    # calculate start and finish sample for current segment
-                    start = samples_per_segment * d
-                    finish = start + samples_per_segment
+                        # calculate start and finish sample for current segment
+                        start = samples_per_segment * d
+                        finish = start + samples_per_segment
 
-                    # extract mfcc
-                    mfcc = librosa.feature.mfcc(y=signal[start:finish], sr=sample_rate, n_mfcc=num_mfcc, n_fft=n_fft, hop_length=hop_length)
-                    mfcc = mfcc.T
+                        # extract mfcc
+                        mfcc = librosa.feature.mfcc(y=signal[start:finish], sr=sample_rate, n_mfcc=num_mfcc, n_fft=n_fft, hop_length=hop_length)
+                        mfcc = mfcc.T
 
-                    # store only mfcc feature with expected number of vectors
-                    if len(mfcc) == num_mfcc_vectors_per_segment:
-                        data["mfcc"].append(mfcc.tolist())
-                        data["labels"].append(i-1)
-                        print("{}, segment:{}".format(file_path, d+1))
+                        # store only mfcc feature with expected number of vectors
+                        if len(mfcc) == num_mfcc_vectors_per_segment:
+                            data["mfcc"].append(mfcc.tolist())
+                            data["labels"].append(i-1)
+                            print("{}, segment:{}".format(file_path, d+1))
+                except Exception as e:
+                    log["failCount"] += 1
+                    log["failed"].append({"file": file_path, "exception": e})
+                    
 
     # save MFCCs to json file
     with open(json_path, "w") as fp:
         json.dump(data, fp, indent=4)
+    with open(log_path, "w") as fp:
+        json.dump(log, fp, indent=4)
 
 
 if __name__ == "__main__":
-    save_mfcc(DATASET_PATH, JSON_PATH, num_segments=10)
+    save_mfcc(DATASET_PATH, JSON_PATH, LOG_PATH, num_segments=10)
