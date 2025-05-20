75a3836372b9f01e9cca3b104d7b3e02058ea9da
Alexander Walsh <91561129+AlexWalsh2@users.noreply.github.com>
2025-05-20 16:19:23 -0700

    Fixed install.sh
    Ensured that install.sh adds brew to the path (so the system actually finds the python3.12), and fixed the cd command, ensuring that the installer can successfully create the venv

--- UI/install.sh
+++ UI/install.sh
@@ -1,15 +1,20 @@
 # install brew and, using that python 3.12, may wish to refine for unattended install later
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew update --force --quiet
-brew install python@3.12
+../linuxbrew/.linuxbrew/bin/brew update --force --quiet
+../linuxbrew/.linuxbrew/bin/brew install python@3.12
+
+# add brew and whatnot to path, drawn from brew's own instructions, provided when you run the installer
+echo >> ~/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # get the project
 git clone https://github.com/beatsageo/Top_n.git
-cd ./UI/
+cd ./Top_n/UI/
 
 # create a venv and install requirements into it for the project
 python3.12 -m venv venv
-source ./bin/activate
+source ./venv/bin/activate
 pip install -r ./requirements.txt
 chmod +x ./findGenre
 deactivate
